d3c7407953d66b0df73234ab454cbe8438a4c2a1
Jonathan Dotore <jonathandotore@outlook.com>
2023-09-28 11:46:07 -0300

    Adding the JS behavior on the page.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,16 @@
-function randomNumber() {
-    let number1 = Math.ceil(Math.random() * 6);
-    let number2 = Math.ceil(Math.random() * 6);
-    let text = document.querySelector('button').textContent;
-    text === ' Play! ' ? ' Re-roll! ' : 'Try Again!';
+let number1 = Math.ceil(Math.random() * 6);
+let number2 = Math.ceil(Math.random() * 6);
+let randomDiceImage = "dice" + number1 + '.png';
+let randomDiceImage2 = "dice" + number2 + '.png';
+let randomImageSource = "./assets/images/" + randomDiceImage;
+let randomImageSource2 = "./assets/images/" + randomDiceImage2;
+let image1 = document.querySelector('.dice1').setAttribute('src', randomImageSource);
+let image2 = document.querySelector('.dice2').setAttribute('src', randomImageSource2);
 
-    return number1, number2;
+if (number1 > number2) {
+    document.querySelector('h1').innerHTML = '🚩 Player 1 Wins!'
+} else if (number2 > number1) {
+    document.querySelector('h1').innerHTML = '🚩 Player 2 Wins!'
+} else if (number1 === number2) {
+    '🙅🏼‍♂️ Game Tied!'
 }
-
-
